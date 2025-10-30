323375566ac16ff991593c658f03b4b0c49d4376
NicoloRomagna <nicolo.romagna@studenti.isissgobetti.it>
2025-10-30 17:58:05 +0100

    primo commit

---
+++ veicolo.py
@@ -0,0 +1,16 @@
+class Veicolo:
+    def __init__(self, marca, modello, anno):
+        self.marca = marca
+        self.modello = modello
+        self.anno = anno
+
+    def descrizione(self):
+        return f"{self.marca} {self.modello}, Anno: {self.anno}"
+    
+class Auto(Veicolo):
+    def __init__(self, marca, modello, anno, serbatoio):
+        super().__init__(marca, modello, anno)
+        self.serbatoio = serbatoio
+
+    def rifornimento(self, litri):
+        serbatoio += litri 
